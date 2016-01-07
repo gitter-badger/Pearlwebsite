7f547a19a16d6c4988dba697902b6b2381cf312d
ElGenius <abdoolly@gmail.com>
2016-01-07 20:01:58 +0200

    PEARL

--- resources/views/pages/profile-page.blade.php
+++ resources/views/pages/profile-page.blade.php
@@ -64,39 +64,8 @@
                     </div>
                 </div>
 <!--Sign Up -->
-                <div class="modal fade" id="SignUp" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title" id="myModalLabel">LOGIN RIGHT HERE</h4>
-                            </div>
-                            <div class="modal-body">
-                                <form role="form">
-
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="username">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="E-mail">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="password" class="form-control" placeholder="Password">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control" placeholder="Re-Password">
-                                    </div>
-
-                                </form>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Creat</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+               @include('layout.signup')
+<!--Sign Up end -->
 
  <nav class="navbar navbar-default">
         <div class="container">
