45b69d92a1972bfcb7b98a058d8325ef28b85f9b
ElGenius <abdoolly@gmail.com>
2016-01-07 10:12:50 +0200

    PEARL

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -11,41 +11,41 @@ class RouteController extends Controller
 {
     public function home()
     {
-        return view('main');
+        return view('pages.main');
     }
     
     public function gallery()
     {
-        return view('images');
+        return view('pages.images');
     }
     
     public function reserve()
     {
-        return view('reservation-form');
+        return view('pages.reservation-form');
     }
     
     public function admin()
     {
-        return view('admin-page');
+        return view('pages.admin-page');
     }
     
     public function edit()
     {
-        return view('edit-reservation');
+        return view('pages.edit-reservation');
     }
     
     public function manager()
     {
-        return view('manager');
+        return view('pages.manager');
     }
     
     public function profile()
     {
-        return view('profile-page');
+        return view('pages.profile-page');
     }
     
     public function receptionist()
     {
-        return view('receptionist');
+        return view('pages.receptionist');
     }
 }
